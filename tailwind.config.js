0e52d6c28ccd9ff8d6343ce4149bb8c9bf04c6c0
Swati Bhatia <swatibhatia626@gmail.com>
2025-10-25 15:58:05 +0530

    daisyUI + navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
 
+/** @type {import('tailwindcss').Config} */
 export default {
- content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [daisyui],
+};
